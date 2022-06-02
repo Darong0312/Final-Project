1723d22b8823462fd2b90a6d8ca92c9a308d1772
yokijyfr <103284162+yokijyfr@users.noreply.github.com>
2022-06-01 17:52:17 -0700

    change to start with tutorial

--- src/Stage/Menu.js
+++ src/Stage/Menu.js
@@ -18,8 +18,8 @@ class Menu extends Phaser.Scene {
         if(Phaser.Input.Keyboard.JustDown(keySpace)){
             this.sound.play('select');
             //this.scene.start("tutorialScene");
-            this.scene.start("stageTwo");
-            //this.scene.start("tutorialScene");
+            //this.scene.start("stageTwo");
+            this.scene.start("tutorialScene");
         }
     }
 }
