453212ccbb335b91cc732d26f370506d9169af0f
Annkenn <anlkenne@ucsc.edu>
2022-06-02 16:16:23 -0700

    adjusted crab animations

--- src/Prefabs/Player2.js
+++ src/Prefabs/Player2.js
@@ -11,6 +11,33 @@ class Player2 extends Phaser.Physics.Arcade.Sprite{
 
         this.interact = false;
 
+
+        //player 2 idle right
+        this.anims.create({
+            key: 'crab_idle_right',
+            frames: this.anims.generateFrameNames('crab_atlas', {
+                prefix: 'crab_jump_right_',
+                start: 1,
+                end: 1,
+                suffix: '',
+                zeroPad: 4
+            }),
+            frameRate: 2,
+            repeat: -1
+        })
+        //player 2 idle left
+        this.anims.create({
+            key: 'crab_idle_left',
+            frames: this.anims.generateFrameNames('crab_atlas', {
+                prefix: 'crab_jump_left_',
+                start: 1,
+                end: 1,
+                suffix: '',
+                zeroPad: 4
+            }),
+            frameRate: 2,
+            repeat: -1
+        })
         // player 2 run right
         this.anims.create({
             key: 'crab_run_right',
@@ -37,17 +64,89 @@ class Player2 extends Phaser.Physics.Arcade.Sprite{
             frameRate: 15,
             repeat: -1
         });
+        // player 2 jump left
+        this.anims.create({
+            key: 'crab_jump_left',
+            frames: this.anims.generateFrameNames('crab_atlas', {
+                prefix: 'crab_jump_left_',
+                start: 2,
+                end: 2,
+                suffix: '',
+                zeroPad: 4
+            }),
+            frameRate: 2,
+            repeat: 0
+        });
+        // player 2 jump right
+        this.anims.create({
+            key: 'crab_jump_right',
+            frames: this.anims.generateFrameNames('crab_atlas', {
+                prefix: 'crab_jump_right_',
+                start: 2,
+                end: 2,
+                suffix: '',
+                zeroPad: 4
+            }),
+            frameRate: 2,
+            repeat: 0
+        });
+        // player 2 push left
+        this.anims.create({
+            key: 'crab_push_left',
+            frames: this.anims.generateFrameNames('crab_atlas', {
+                prefix: 'crab_push_left_',
+                start: 1,
+                end: 2,
+                suffix: '',
+                zeroPad: 4
+            }),
+            frameRate: 8,
+            repeat: 0
+        });
+        // player 2 push right
+        this.anims.create({
+            key: 'crab_push_right',
+            frames: this.anims.generateFrameNames('crab_atlas', {
+                prefix: 'crab_push_right_',
+                start: 1,
+                end: 2,
+                suffix: '',
+                zeroPad: 4
+            }),
+            frameRate: 8,
+            repeat: 0
+        });
     }
 
     update(){
         this.setVelocityX(0);
+        if (!this.anims.isPlaying) {
+            this.anims.play('crab_idle_right', true);
+        }
+
+        if(!keyLEFT.isDown && !keyRIGHT.isDown && this.anims.isPlaying) {
+            if(this.anims.currentAnim.key === 'crab_run_left') {
+                this.anims.play('crab_idle_left', true);
+            }
+            else if (this.anims.currentAnim.key === 'crab_run_right') {
+                this.anims.play('crab_idle_right', true);
+            }
+        }
+        else if (keyLEFT.isUp && keyRIGHT.isUp && this.body.touching.down) {
+            this.anims.play('crab_idle_right', true);
+        }
+
         if(keyLEFT.isDown){
             this.setVelocityX(-speed);
-            this.anims.play('crab_run_left', true);
+            if (this.body.touching.down) {
+                this.anims.play('crab_run_left', true);
+            }
         }
         else if(keyRIGHT.isDown){
             this.setVelocityX(speed);
-            this.anims.play('crab_run_right', true);
+            if (this.body.touching.down) {
+                this.anims.play('crab_run_right', true);
+            }
         }
 
         if(keyUP.isDown){
@@ -56,6 +155,20 @@ class Player2 extends Phaser.Physics.Arcade.Sprite{
                     this.setVelocityY(-300);
                     this.jump = true;
                     --this.jumpCount;
+                    if (keyRIGHT.isDown) {
+                        this.anims.play('crab_jump_right', true);
+                    }
+                    else if (keyLEFT.isDown) {
+                        this.anims.play('crab_jump_left', true);
+                    }
+                    else {
+                        if (this.anims.currentAnim.key === 'crab_idle_right') {
+                            this.anims.play('crab_jump_right', true);
+                        }
+                        else if (this.anims.currentAnim.key === 'crab_idle_left') {
+                            this.anims.play('crab_jump_left', true);
+                        }
+                    }
                 }
             }
         }
